b6581d11b2d4f288745526ea79f754542c351273
Junghoon Noh <jungnoh.dev@gmail.com>
2020-06-11 12:36:03 +0900

    feat(layout): setup drawer and menu content

--- frontend/src/components/templates/Page.tsx
+++ frontend/src/components/templates/Page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { AppBar, Drawer, Toolbar, Typography, IconButton, MenuItem, Menu } from '@material-ui/core';
-import { Menu as MenuIcon, AccountCircle } from '@material-ui/icons';
+import { AppBar, Drawer, Toolbar, Typography, IconButton, MenuItem, Menu, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Menu as MenuIcon, AccountCircle, Home as HomeIcon, Person as PersonIcon } from '@material-ui/icons';
 import styles from './Page.scss';
+import { Link } from 'react-router-dom';
 
 interface PageProps {
   children: JSX.Element;
@@ -12,12 +13,23 @@ export default function PageTemplate(props: PageProps) {
   const open = Boolean(anchorEl);
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   
+  const isLoggedIn = sessionStorage.getItem('username') !== null;
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleMenuClose = () => {
     setAnchorEl(null);
   };
+
+  const userMenu = (<>
+    <MenuItem><Link to="/my" className={styles.menuItem}>마이페이지</Link></MenuItem>
+    <MenuItem><Link to="/auth/logout" className={styles.menuItem}>로그아웃</Link></MenuItem>
+  </>);
+  const guestMenu = (<>
+    <MenuItem><Link to="/auth/login" className={styles.menuItem}>로그인</Link></MenuItem>
+    <MenuItem><Link to="/auth/join" className={styles.menuItem}>회원가입</Link></MenuItem>
+  </>);
+
   return (
     <div className={styles.root}>
       <AppBar position="fixed" style={{zIndex: 9999}} className={styles.appbar}>
@@ -52,15 +64,29 @@ export default function PageTemplate(props: PageProps) {
               }}
               open={open}
               onClose={handleMenuClose}
+              style={{zIndex: 100000}}
             >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>My account</MenuItem>
+              {isLoggedIn ? userMenu : guestMenu}
             </Menu>
           </div>
         </Toolbar>
       </AppBar>
       <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)} className={styles.drawer}>
-       
+        <div className={styles.drawerSpacing} />
+        <List>
+          <Link to="/" className={styles.menuItem}>
+            <ListItem button>
+              <ListItemIcon><HomeIcon /></ListItemIcon>
+              <ListItemText primary="메인 화면" />
+            </ListItem>
+          </Link>
+          <Link to="/me" className={styles.menuItem}>
+            <ListItem button>
+              <ListItemIcon><PersonIcon /></ListItemIcon>
+              <ListItemText primary="마이페이지" />
+            </ListItem>
+          </Link>
+        </List>
       </Drawer>
       <div className={styles.child}>
         {props.children}
